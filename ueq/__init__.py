b8d9405a4617f6a172d9515286fd3f33ac860ad0
Kiplangat Korir <korirkiplangat22@gmail.com>
2025-09-22 15:28:04 +0300

    Updated init.py

--- ueq/__init__.py
+++ ueq/__init__.py
@@ -1,5 +1,10 @@
 from .core import UQ
-from .utils.metrics import coverage, sharpness, expected_calibration_error, maximum_calibration_error
+from .utils.metrics import (
+    coverage,
+    sharpness,
+    expected_calibration_error,
+    maximum_calibration_error
+)
 
 __version__ = "0.1.0"
 __all__ = [
